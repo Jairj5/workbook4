a4a6db0808d7d1e950e631e5e151b0b4142f3236
Jair Jabriel <JJabriel@atl.yearup.org>
2022-10-11 16:01:20 -0400

    last change

---
+++ exercises/arrays.js/my_family.js
@@ -0,0 +1,9 @@
+let myFamily = ["Jair", "Sonya","Noah","Kami"];
+
+let familyLenght = myFamily.length
+
+for (let i = 0; i < familyLenght; i++) {
+    const element = myFamily[i];
+    console.log(myFamily[i]);
+    
+}
